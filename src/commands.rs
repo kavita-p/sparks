7e75ce8a6c0b8b607fea29ea192116a96cb84d7b
Kavita Poduri <76273926+kavita-p@users.noreply.github.com>
2025-10-23 09:45:54 -0400

    remove deckbuilding crates

--- src/commands.rs
+++ src/commands.rs
@@ -1,20 +1,2 @@
-use core::fmt;
-use std::fmt::Display;
-
-#[derive(poise::ChoiceParameter)]
-enum Deck {
-    Standard,
-    Tarot,
-}
-
-impl Display for Deck {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::Standard => write!(f, "standard"),
-            Self::Tarot => write!(f, "tarot"),
-        }
-    }
-}
-
 pub mod misc;
 pub mod roll;
